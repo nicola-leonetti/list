a8f4cabec3707b6ef512a8c0b9adb53901c83684
Nicola Leonetti <Nicola@MacBook-Air.home-life.hub>
2021-12-28 12:13:55 +0100

    Aggiungo file di configurazione di VSCode e supporto per gli algoritmi di ordinamento

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include "src/list.h"
+#define DIM 9
 
 int main(void)
 {
     list l; 
-    l = emptylist(); 
+    element arrayDiElementi[DIM] = {-1, 5, 7, 4, -3, -3, 9, 11, -12};
+
+    l = emptyList(); 
 
     printf("%d\n", empty(l));
 
@@ -15,6 +18,15 @@ int main(void)
 
     printElement(head(l));
     printList(tail(l));
+
+    printElementArray(arrayDiElementi, DIM);
+    // naiveSort(arrayDiElementi, DIM);
+    // bubbleSort(arrayDiElementi, DIM);
+    // insertSort(arrayDiElementi, DIM);
+    //element arrayOrdinato[DIM];
+    // mergeSort(arrayDiElementi, 0, DIM - 1, arrayOrdinato);
+    quickSort(arrayDiElementi, DIM);
+    printElementArray(arrayDiElementi, DIM);
 
     return 0;
 }
--- src/element.h
+++ src/element.h
@@ -7,7 +7,28 @@ typedef int element;
 
 int compare(element e1, element e2);
 
-// Stampa un elemento SU UNA SOLA RIGA
 void printElement(element e);
+
+void printElementArray(element e[], int dim);
+
+void scambia(element *e1, element *e2);
+
+int trovaPosMax(element e[], int dim);
+
+void insOrd(element e[], int pos);
+
+void merge(int v[], int i1, int i2, int fine, int eout[]);
+
+void quickSortR(int e[], int iniz, int fine);
+
+void naiveSort(element e[], int dim);
+
+void bubbleSort(element e[], int dim);
+
+void insertSort(element e[], int dim);
+
+void mergeSort(element e[], int first, int last, element eout[]);
+
+void quickSort(element e[], int dim);
 
 #endif
